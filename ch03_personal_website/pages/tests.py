8bdb6bdbbc0e4d21b23113bf19a5aa39755738fc
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-19 21:35:14 +0100

    completed chapter 3

--- ch03_personal_website/pages/tests.py
+++ ch03_personal_website/pages/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
 # Create your tests here.
+"""Write the test"""
+
+
+class HomepageTest(SimpleTestCase):
+    def test_home_page_returns_successful(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutpageTest(SimpleTestCase):
+    def test_about_page_returns_successful(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
