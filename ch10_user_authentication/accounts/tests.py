e7befa2638b698dad2491f1be923ccf0c3caec6a
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-24 11:50:27 +0100

    Test sign up page functionalities
    test url exist, signup form, view name

--- ch10_user_authentication/accounts/tests.py
+++ ch10_user_authentication/accounts/tests.py
@@ -2,6 +2,7 @@
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your tests here.
 
 
@@ -31,3 +32,32 @@ class UsersManagersTests(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignupPageTests(TestCase):
+    def test_url_exist_at_correct_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def signup_form(self):
+        response = self.client.post(
+            reverse(
+                "signup",
+                {
+                    "username": "testuser3",
+                    "email": "testuser3@example.com",
+                    "password": "testpassword",
+                },
+            )
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser3")
+        self.assertEqual(
+            get_user_model().objects.all()[0].email, "testuser3@example.com"
+        )
