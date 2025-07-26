69209cf5607efc18d622b9569df4d9e09cd5df06
Clinton Odia <odiamehiclinton@gmail.com>
2025-07-26 19:50:23 +0100

    Update article urls and view

--- ch13_articles_app/articles/admin.py
+++ ch13_articles_app/articles/admin.py
@@ -3,4 +3,9 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "body", "author"]
+
+
+admin.site.register(Article, ArticleAdmin)
